3395875ee1ea5e1e60e27d179bec67b57fece0b2
SampaiWasTaken <samuel.radler@htl-klu.at>
2024-04-13 14:14:45 +0200

    Combat

--- Scripts/Combat/bullet.gd
+++ Scripts/Combat/bullet.gd
@@ -1,13 +1,16 @@
-extends Area2D
-@export var speed = 20
+class_name Bullet
 
+extends Area2D
+
+@export var speed = 20
+@export var oppositionString = ""
 var direction = Vector2.ZERO
 
 func _physics_process(delta):
 	position += direction*speed
 
 func _on_body_entered(body:Node2D):
-	if body.is_in_group("MiniPlayer"):
+	if body.is_in_group(oppositionString):
 		body.hurt()
 		queue_free()
 	elif(body.is_in_group("Walls")):
