9d97674a6c6bce1d1ac2ec57673888b8ffe8164b
SampaiWasTaken <samuel.radler@htl-klu.at>
2024-04-14 13:42:44 +0200

    added gas attack

--- Scripts/Combat/boulder.gd
+++ Scripts/Combat/boulder.gd
@@ -8,7 +8,6 @@ func _ready():
 	
 
 func _on_body_entered(body):
-	print("bruh")
 	if body.is_in_group("MiniPlayer"):
 		body.hurt()
 		queue_free()
--- Scripts/Combat/combat_scene_bat.gd
+++ Scripts/Combat/combat_scene_bat.gd
@@ -1,9 +1,11 @@
 extends combatScene
 
 var obj_boulder = preload("res://Scenes/CombatScenes/boulder.tscn")
+var obj_gas = preload("res://Scenes/CombatScenes/bad_gas.tscn")
 
 func _ready():
 	$BoulderTimer.start()
+	$GasTimer.start()
 	
 func _on_boulder_timer_timeout():
 	var boulder = obj_boulder.instantiate()
@@ -22,3 +24,20 @@ func _on_boulder_timer_timeout():
 	
 
 	add_child(boulder)
+
+
+func _on_gas_timer_timeout():
+	var dir = global_position.direction_to($player.global_position)
+	shoot(150)
+	$Path2D/PathFollow2D.progress_ratio = randf()
+	
+func shoot(speed: float):
+	print("gas")
+	var gas = obj_gas.instantiate()
+	var loc = $Path2D/PathFollow2D/GasSpawn.global_position
+	gas.position = loc
+	gas.scale = Vector2(0.1, 0.1)
+	gas.velocity = Vector2(speed, 0)
+	
+	$"..".add_child(gas)
+	$GasTimer.start(randi_range(2,7))
