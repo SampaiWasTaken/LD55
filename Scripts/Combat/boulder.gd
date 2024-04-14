5ee39f248770fd9ac359fb0928abbdec874db8b9
SampaiWasTaken <samuel.radler@htl-klu.at>
2024-04-14 15:12:40 +0200

    pfusch

--- Scripts/Combat/boulder.gd
+++ Scripts/Combat/boulder.gd
@@ -1,11 +1,15 @@
 extends RigidBody2D
-
+var ttl = 5
 
 func _ready():
 	connect("body_entered",  _on_body_entered)
 	contact_monitor = true
 	set_max_contacts_reported(1)
-	
+
+func _process(delta):
+	ttl -= delta
+	if ttl <= 0:
+		queue_free()
 
 func _on_body_entered(body):
 	if body.is_in_group("MiniPlayer"):
--- Scripts/Combat/tutorial_combat.gd
+++ Scripts/Combat/tutorial_combat.gd
@@ -2,3 +2,4 @@ extends combatScene
 
 func _ready():
 	$player.canAct = false
+	#$Walls/StaticBody2D5.disable()
