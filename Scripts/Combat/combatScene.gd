2637c6919517b995f46cd11b2a7d27306f71997b
SampaiWasTaken <samuel.radler@htl-klu.at>
2024-04-15 16:47:00 +0200

    Added new overworld interactables and secret boss

--- Scripts/Combat/combatScene.gd
+++ Scripts/Combat/combatScene.gd
@@ -1,7 +1,7 @@
 class_name combatScene
 extends Node2D
 
-@export var Player: Node
+@onready var Player = get_tree().get_first_node_in_group("MiniPlayer")
 
 func _process(_delta):
 	$UI/HPBar.value = Player.health
