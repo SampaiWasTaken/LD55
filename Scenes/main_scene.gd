13e1925925df232fa048ff6b780eef476938bfaf
MrMerlinH <67464744+MrMerlinH@users.noreply.github.com>
2024-04-15 14:39:50 +0200

    added forest, cave, light, collisions, lantern pickup

--- Scenes/main_scene.gd
+++ Scenes/main_scene.gd
@@ -5,9 +5,32 @@ func _ready():
 	var mat1 = load("res://Scenes/shader test/2dShadowShaderMat.tres")
 	var mat2 = load("res://Scenes/shader test/2dShadowShaderMat2.tres")
 	var trees = $Trees.get_children()
-	for tree in trees:
-		if tree.scale.y < 0:
-			tree.material = mat1
-		else:
-			tree.material = mat2
+	
+	if "lantern" in Globals.player_items:
+		$InteractableSquare.queue_free()
+	#for tree in trees:
+	#	if tree.scale.y < 0:
+		#	tree.material = mat1
+		#else:
+		#	tree.material = mat2
 
+
+
+func _on_area_2d_body_entered(body):
+	var tween = get_tree().create_tween()
+	tween.set_parallel(true)
+	tween.tween_property($DirectionalLight2D, "energy", 0.7, 2)
+	if "lantern" in Globals.player_items:
+		$MainChar/PointLight2D.show()
+		tween.tween_property($MainChar/PointLight2D, "energy", 1, 2)
+	pass # Replace with function body.
+
+
+func _on_area_2d_body_exited(body):
+	var tween = get_tree().create_tween()
+	tween.set_parallel(true)
+	tween.tween_property($DirectionalLight2D, "energy", 0, 2)
+	tween.tween_property($MainChar/PointLight2D, "energy", 0, 2)
+	await tween.finished
+	$MainChar/PointLight2D.hide()
+	pass # Replace with function body.
